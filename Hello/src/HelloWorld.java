2479ba06b3acf1dbe26e75e13798d34c18075110
walkers2 <walkers2@appstate.edu>
2016-01-15 13:18:07 -0500

    First Commit

---
+++ Hello/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author walkers2
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World\n");
+	}
+
+}
