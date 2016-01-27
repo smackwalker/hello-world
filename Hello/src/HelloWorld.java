1d1808cccd0b54d24bcaf49e82d4b015a8913a20
walkers2 <walkers2@CS-ABH309-21.appd.appstate.edu>
2016-01-27 13:34:16 -0500

    Test now uses Assert and looks for my name

--- Hello/src/HelloWorld.java
+++ Hello/src/HelloWorld.java
@@ -13,7 +13,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello World\n");
+		System.out.print("Hello Scott\n");
 	}
 
 }
--- Hello/src/HelloWorldTest.java
+++ Hello/src/HelloWorldTest.java
@@ -1,13 +1,49 @@
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 import org.junit.Test;
 
 
 public class HelloWorldTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws IOException {
+		File file = new File("Out.txt");
+		FileOutputStream fOutputStream = new FileOutputStream(file);
+		PrintStream pStream = new PrintStream(fOutputStream);
+		System.setOut(pStream);
+		HelloWorld.main(null);
+		Scanner keyboard = new Scanner(file);
+		int test = 0;
+		boolean pass = false;
+		assertEquals(keyboard.next(),"Hello");
+		assertEquals(keyboard.next(),"Scott");
+//		while (keyboard.hasNext())
+//		{
+//			if (test == 0)
+//			{
+//				keyboard.next().equals("Hello");
+//				test++;
+//			}
+//			else
+//			{
+//				keyboard.next().equals("World");
+//				pass = true;
+//			}
+//		}
+		keyboard.close();
+		fOutputStream.close();
+//		if (!pass)
+//		{
+//			fail("Not yet implemented");
+//		}
+		System.setOut(System.out);
 	}
 
 }
